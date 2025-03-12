9c5e432382e2892fa40f8f3dd942b3c0a19b6cdd
Alex (MS) <74899889+olekkut@users.noreply.github.com>
2025-03-12 13:41:28 +0100

    Add exercise 17 for Python basics

---
+++ basics/zad17.py
@@ -0,0 +1,10 @@
+# Zadanie 17. Użycie zmiennych w tekście
+# Pobierz nazwę ulubionego miasta i kraju.
+# Wyświetl zdanie:
+# `Moje ulubione miasto to [miasto], które znajduje się w [kraj].`\
+
+favourite_city = input('Proszę wprowadzić ulubione miasto')
+favourite_country = input('Proszę wprowadzić ulubiony kraj')
+
+
+# TODO print()
